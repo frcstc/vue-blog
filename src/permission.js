b8ad573d85604bac5d12accfc7ce552d8885f05b
frc <343663190@qq.com>
2020-06-04 09:44:24 +0800

    no message

--- src/permission.js
+++ src/permission.js
@@ -6,9 +6,18 @@ router.beforeEach((to,from,next) => {
         const token = localStorage.getItem("token")
         if(token) {
             if (to.path === '/login') {
-
+                next({
+                    path: '/login'
+                })
             } else {
-                next()
+                const session = sessionStorage.getItem('userInfo')
+                if(session) {
+                    next()
+                } else {
+                    next({
+                        path: '/login'
+                    })
+                }
             }
         } else {
             next({
